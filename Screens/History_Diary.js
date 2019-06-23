eef8da706cbb52ddebb3b130a66ef9cfc92aa683
NestZ <NestZ671@gmail.com>
2019-06-23 23:31:56 +0700

    add AsyncStorage

--- Screens/History_Diary.js
+++ Screens/History_Diary.js
@@ -1,22 +1,26 @@
 import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 import React, {Component} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
+/*
+GLOBAL VAR
+-count per day
+  -date
+  -count
+  -all timestamp
+*/
 
 dateList = [
-    {
-      id : 1,
-      date : "12/01/61",
-      count : 8
-    },
-    {
-      id : 2,
-      date : "5/8/61",
-      count : 12
-    },
-    {
-      id : 3,
-      date : "15/9/61",
-      count : 5
-    }
+  {
+    id : 1,
+    date : "12/01/61",
+    count : 8
+  },
+  {
+    id : 2,
+    date : "5/8/61",
+    count : 12
+  }
 ];
   
 const styles = StyleSheet.create({
@@ -40,8 +44,16 @@ function renderDate(navigate){
     });
     return dateArr;
 }
-  
+
 class History_Diary extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      time: '',
+      counter : 0,
+      nowTime : ''
+    }
+  }
   static navigationOptions = ({ navigation }) => {
     return {
        header: () => null
@@ -52,8 +64,65 @@ class History_Diary extends React.Component{
     return (
       <View>
         {renderDate(navigate)}
+        <TouchableOpacity onPress={() => this.plusCounter()}>
+          <Text>PLUS COUNTER</Text>
+        </TouchableOpacity>
+        <Text>
+          counter : {this.state.counter} Time : {this.state.time}
+        </Text>
+        <Text>
+          {this.state.nowTime}
+        </Text>
       </View>
     );
+  }
+  plusCounter = async() => {
+    await this.getTime()
+    this.pushData();
+  }
+  getTime = async() => {
+    var date = new Date().getDate();
+    var month = new Date().getMonth();
+    var year = new Date().getFullYear();
+    var hour = new Date().getHours();
+    var minute = new Date().getMinutes();
+    this.setState({counter:++this.state.counter});
+    this.setState({time:
+      date + '/' + month + '/' + year + ' ' + hour + ':' + minute
+    });
+  }
+  pushData = async() => {
+    //await AsyncStorage.removeItem('timedata')
+    const dataToBeSaved = {
+      currCount : this.state.counter,
+      timer : this.state.time
+    }
+    const existingData = await AsyncStorage.getItem('timedata')
+    let newData = JSON.parse(existingData);
+    if(newData == null){
+      newData = [];
+    }
+    newData.push(dataToBeSaved);
+    this.setData('timedata',JSON.stringify(newData));
+    /*const data = await AsyncStorage.getItem('timedata')
+    let lastData = JSON.parse(data);
+    this.setState({nowTime:lastData[0].timer});*/
+  }
+  setData = async(key, value) => {
+    try{
+      await AsyncStorage.setItem(key,value)
+    }
+    catch(e){
+      return;
+    }
+  }
+  getData = async() => {
+    try {
+      await AsyncStorage.getItem('timedata')
+    }
+    catch(e){
+      return;
+    }
   }
 }
 
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,13 +1,57 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 class Home extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      counter : 0,
+      time : '',
+      nowTime : ''
+    }
+  }
   render() {
     return (
       <View>
-        <Text>Home</Text>
+        <TouchableOpacity onPress={() => this.plusCounter()}>
+          <Text>PLUS COUNTER</Text>
+        </TouchableOpacity>
+        <Text>Counter : {this.state.counter} Time : {this.state.time}</Text>
       </View>
     );
+  }
+  plusCounter = async() => {
+    await this.getTime()
+    this.pushData();
+  }
+  getTime = async() => {
+    var date = new Date().getDate();
+    var month = new Date().getMonth();
+    var year = new Date().getFullYear();
+    var hour = new Date().getHours();
+    var minute = new Date().getMinutes();
+    this.setState({counter:++this.state.counter});
+    this.setState({time:
+      date + '/' + month + '/' + year + ' ' + hour + ':' + minute
+    });
+  }
+  pushData = async() => {
+    //await AsyncStorage.removeItem('timedata')
+    const dataToBeSaved = {
+      currCount : this.state.counter,
+      timer : this.state.time
+    }
+    const existingData = await AsyncStorage.getItem('timedata')
+    let newData = JSON.parse(existingData);
+    if(newData == null){
+      newData = [];
+    }
+    newData.push(dataToBeSaved);
+    this.setData('timedata',JSON.stringify(newData));
+  }
+  setData = async(key,value) => {
+    await AsyncStorage.setItem(key,value);
   }
 }
 
