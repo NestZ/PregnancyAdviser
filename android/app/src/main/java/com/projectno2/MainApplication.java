83d6dae992f30262ce5b84c73a14453121be0392
NestZ <NestZ671@gmail.com>
2019-06-29 16:50:08 +0700

    last Worked

--- android/app/src/main/java/com/projectno2/MainApplication.java
+++ android/app/src/main/java/com/projectno2/MainApplication.java
@@ -3,6 +3,7 @@ package com.projectno2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSoundPlayerPackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage()
       );
