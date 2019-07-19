0c004412b719d3b8e81c38d8863197bfaf5647a9
wisaitas <46591028+wisaitas@users.noreply.github.com>
2019-07-19 11:17:52 +0700

    Update Details1.js

--- Screens/details/Details1.js
+++ Screens/details/Details1.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 
 class Details1 extends React.Component{
   static navigationOptions = ({ navigation }) => {
@@ -10,11 +10,42 @@ class Details1 extends React.Component{
   render() {
   const {navigate} = this.props.navigation;
   return (
-    <Text>
-      <Text>My Details1</Text>
-    </Text>
+    <View style={styles.container}>
+      <View style={styles.header}> 
+          <Text style={styles.headerText}> การนับลูกดิ้น </Text>
+      </View>
+      <View style={styles.frame}>
+        <Text style={styles.Text}>            ถือเป็นวิธีการประเมินสุขภาพลูกในท้อง ที่คุณแม่สามารถประเมินสุขภาพลูกน้อยในท้องและปฏิบัติได้ </Text>
+        <Text style={styles.Text}>ด้วยตนเอง  และช่วยเฝ้าระวังเกี่ยวกับภาวะอันตรายลูกในท้องซึ่งจะมีการเปลี่ยนแปลงพฤติกรรม การดิ้นที่คุณ</Text>
+        <Text style={styles.Text}>แม่สามารถสังเกตได้ ดังนั้นเมื่อคุณแม่อายุครรภ์เข้าสู่ 32 สัปดาห์ขึ้นไป ควรนับลูกดิ้นทุกวันโดยเฉพาะในคุณ </Text>
+        <Text style={styles.Text}>แม่ที่ได้รับการวินิจฉัยว่าตั้งครรภ์เสี่ยงสูง </Text>
+      </View>
+    </View>
   );
   }
 }
 
 export default Details1;
+
+const styles = StyleSheet.create ({
+  container:{
+    flex: 1,
+    backgroundColor: "#ffcc99",
+  },
+  header:{
+    backgroundColor: '#E91E63',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText:{
+    color: 'white',
+    fontSize: 24,
+    padding: 15,
+  },
+  Text:{
+    fontSize: 18,
+  },
+  frame:{
+    backgroundColor: "#ffcc99",
+  },
+})
