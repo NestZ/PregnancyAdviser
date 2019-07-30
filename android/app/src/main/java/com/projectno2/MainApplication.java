407e0aafe4c15d8e5fa9cd345f742811eac405e4
NestZ <NestZ671@gmail.com>
2019-07-30 18:11:17 +0700

    bugs fixed

--- android/app/src/main/java/com/projectno2/MainApplication.java
+++ android/app/src/main/java/com/projectno2/MainApplication.java
@@ -3,6 +3,7 @@ package com.projectno2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNSoundPlayerPackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage(),
